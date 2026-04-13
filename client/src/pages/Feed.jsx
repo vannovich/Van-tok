c5b4b54a173edac27d5d9bc313e180c59cad6ce4
vannovich <awontuvannovich@gmail.com>
2026-04-13 17:37:27 +0100

    Feed UI right side Completed

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { dummyPostsData } from "../assets/assets";
+import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
 import PostCard from "../components/PostCard";
+import RecentMessages from "../components/RecentMessages";
 
 function Feed() {
   const [feeds, setFeeds] = useState([]);
@@ -21,16 +22,22 @@ function Feed() {
         <StoriesBar />
         <div className="p-4 space-y-6">
           {feeds.map((post) => (
-            <PostCard key={post._id | post.id} post={post} />
+            <PostCard key={post._id || post.id} post={post} />
           ))}
         </div>
       </div>
       {/* Right side bar */}
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className="max-xl:block sticky top-0">
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3 className="text-slate-800 font-semibold">Sponsored</h3>
+          <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" />
+          <p className="text-slate-600">Email marketing</p>
+          <p className="text-slate-400">
+            Supercharge your marketing with a powerful, easy-to-use platform
+            built for results.
+          </p>
         </div>
-        <h1>Recent Messages</h1>
+        <RecentMessages/>
       </div>
     </div>
   ) : (
