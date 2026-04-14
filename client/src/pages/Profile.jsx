4b06ef3045e0a36676b23735fcd88e0664352bd2
vannovich <awontuvannovich@gmail.com>
2026-04-14 14:46:12 -0400

    Discover page UI Completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyPostsData, dummyUserData } from "../assets/assets";
+import Loading from "../components/Loading";
+import UserProfileInfo from "../components/UserProfileInfo";
 
 function Profile() {
-  return (
-    <div>
-      Profile
+  const { profileId } = useParams();
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("posts");
+  const [showEdit, setShowEdit] = useState(false);
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+    setPosts(dummyPostsData);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return user ? (
+    <div className="relative h-full overflow-y-scroll bg-gray-50 p-6">
+      <div className="max-w-3xl mx-auto">
+        {/* Profile Card */}
+        <div className="bg-white rounded-2xl shadow overflow-hidden">
+          {/* Cover Photo */}
+          <div className="h-40 md:h-56 bg-linear-to-r from-indigo-200 via-purple-200 to-pink-200">
+            {user.cover_photo && (
+              <img
+                src={user.cover_photo}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            )}
+          </div>
+          {/* User Info */}
+          <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit}/>
+        </div>
+      </div>
     </div>
-  )
+  ) : (
+    <Loading />
+  );
 }
 
-export default Profile
+export default Profile;
